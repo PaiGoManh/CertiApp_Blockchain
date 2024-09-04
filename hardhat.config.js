765d9ba66ead2523b5dde02cc1665b2336f84fef
Rahul Sajeevan <144695000+PaiGoManh@users.noreply.github.com>
2024-09-04 21:51:07 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,10 +7,6 @@ module.exports = {
     localhost: {
       url:"http://127.0.0.1:8545/"
     },
-    infurasepolia: {
-      url :"https://sepolia.infura.io/v3/129df6e60dba4464831b233b9bdd3a00",
-      accounts:["9fdafbea5f9a2c0412427a77c5127734f384ba995a1767eac047aee2001ca9b9"]
-    }
   },
   solidity: "0.8.20",
 };
